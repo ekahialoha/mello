d4d3b51514755787d22627a5d384876d9bb8370c
Christian Kelsom-Martin <github@ekahi.co>
2019-09-22 13:37:51 -0700

    remove boilerplate cards and add list section

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,29 @@ var $boardContainer = document.querySelector('.container');
 
 var board = new Board();
 
-board.addList('To Do');
-board.addList('In Progress');
-board.addList('Done');
+function renderBoard() {
+  board.lists.forEach(function(list) {
+    var $listContainer = document.createElement('div');
+    $listContainer.className = 'list';
 
-board.lists.forEach(function(list, index) {
-  var $listContainer = document.createElement('div');
-  $listContainer.className = 'list';
+    var $header = document.createElement('header');
 
-  var $header = document.createElement('header');
-  var $headerButton = document.createElement('button');
-  $headerButton.textContent = list.title;
+    var $headerButton = document.createElement('button');
+    $headerButton.textContent = list.title;
 
-  $header.appendChild($headerButton);
-  $listContainer.appendChild($header);
-  $boardContainer.appendChild($listContainer);
-});
+    $header.appendChild($headerButton);
+    $listContainer.appendChild($header);
+    $boardContainer.appendChild($listContainer);
+  });
+
+  var $addListContainer = document.createElement('div');
+  $addListContainer.className = 'list add';
+
+  var $addListButton = document.createElement('button');
+  $addListButton.textContent = '+ Add another list';
+
+  $addListContainer.appendChild($addListButton);
+  $boardContainer.appendChild($addListContainer);
+}
+
+renderBoard();
