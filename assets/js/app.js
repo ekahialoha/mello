a399ad1b5677393b0e9b174e615967ab0cb198d2
Christian Kelsom-Martin <github@ekahi.co>
2019-09-22 13:55:16 -0700

    add card to board

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,15 @@ function handleListCreate() {
 }
 
 function handleCardCreate(event) {
-  console.log(event);
+  var $listContainer = event.target.parentNode;
+  var listId = Number($listContainer.getAttribute('data-id'));
+
+  var cardText = prompt('New card text') || '';
+
+  if (cardText.trim()) {
+    board.addCard(listId, cardText);
+    renderBoard();
+  }
 }
 
 function renderBoard() {
@@ -28,12 +36,26 @@ function renderBoard() {
     var $headerButton = document.createElement('button');
     $headerButton.textContent = list.title;
 
+    var $cardUl = document.createElement('ul');
+
+    list.cards.forEach(function(card) {
+      var $cardLi = document.createElement('li');
+
+      var $cardButton = document.createElement('button');
+
+      $cardButton.textContent = card.text;
+
+      $cardLi.appendChild($cardButton);
+    $cardUl.appendChild($cardLi);
+    });
+
     var $addCardButton = document.createElement('button');
     $addCardButton.textContent = 'Add a card...';
     $addCardButton.addEventListener('click', handleCardCreate);
 
     $header.appendChild($headerButton);
     $listContainer.appendChild($header);
+    $listContainer.appendChild($cardUl);
     $listContainer.appendChild($addCardButton);
     $boardContainer.appendChild($listContainer);;
   });
